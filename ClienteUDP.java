2478a928355384948f6c3a1f2f8eb56a7f2eca9e
Eduardo Enes Traunig <e.enes@edu.pucrs.br>
2024-10-02 07:30:03 -0300

    algumas correções, enviar arquivos ainda não funciona

--- ClienteUDP.java
+++ ClienteUDP.java
@@ -1,3 +1,4 @@
+import java.io.*;
 import java.net.*;
 import java.util.Scanner;
 
@@ -33,9 +34,47 @@ public class ClienteUDP {
             Scanner scanner = new Scanner(System.in);
             while (true) {
                 String message = scanner.nextLine();
+                if(message.startsWith("/FILE ")) {
+                    String splitMessage[] = message.substring(6).trim().split(" ");
+                    String fileName = message.substring(6 + splitMessage[0].length()).trim();
+                    File file = new File(fileName);
+
+                    if(!file.exists()) {
+                        System.out.println("Arquivo não encontrado.");
+                        continue;
+                    }
+
+                    byte[] fileBytes = new byte[(int) file.length()];
+                    FileInputStream streamReadFile = new FileInputStream(file);
+                    BufferedInputStream bufferReadFile = new BufferedInputStream(streamReadFile);
+                    bufferReadFile.read(fileBytes, 0, fileBytes.length);
+
+                    int chunkSize = 8192; // 8KB
+                    int numChunks = (int) Math.ceil(fileBytes.length / (double) chunkSize); 
+
+                    message = message + " " + numChunks;
+                    byte[] buffer = message.getBytes();
+                    DatagramPacket sendPacket = new DatagramPacket(buffer, buffer.length, serverAddress, serverPort);
+                    socket.send(sendPacket);
+
+                    for (int i = 0; i < numChunks; i++) {
+                        int start = i * chunkSize;
+                        int length = Math.min(chunkSize, fileBytes.length - start);
+                        byte[] chunk = new byte[length];
+                        System.arraycopy(fileBytes, start, chunk, 0, length);
+        
+                        DatagramPacket packet = new DatagramPacket(chunk, chunk.length, serverAddress, serverPort);
+                        socket.send(packet);
+                    }
+                    streamReadFile.close();
+                    bufferReadFile.close();
+                    System.out.println("File sent successfully.");
+                }
+
                 byte[] buffer = message.getBytes();
                 DatagramPacket sendPacket = new DatagramPacket(buffer, buffer.length, serverAddress, serverPort);
                 socket.send(sendPacket);
+
                 if(message.trim().equals("/FIM")) {
                     System.out.println("Encerrando.");
                     socket.close();
--- ServerUDP.java
+++ ServerUDP.java
@@ -1,3 +1,6 @@
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
 import java.net.*;
 import java.util.*;
 
@@ -29,19 +32,22 @@ public class ServerUDP {
                 // Message
                 String command = new String(receivePacket.getData(), 0, receivePacket.getLength());
                 
-                // user registration
+                // User registration
                 if(command.startsWith("/REG ")) {
                     String userName = command.substring(5).trim().split(" ")[0];
+
+                    // Add the user if it's not already registered
                     if(!clients.contains(clientSocketAddress)) {
                         userNames.put(clientSocketAddress, userName);
                         clients.add(clientSocketAddress);
                     }
+                    // If the user is already registered, print a message
                     else {
                         System.out.println("Usuário já registrado: " + userName + " (" + clientSocketAddress + ")");
                         for(InetSocketAddress client : clients) {
                             if(userNames.get(client).equals(userName)) {
                                 System.out.println(userName + " já está conectado.");
-                                break;
+                                continue;
                             }
                         }
                     }
@@ -49,23 +55,26 @@ public class ServerUDP {
                     continue;
                 }
 
-                // Verificar se o cliente já se registrou com um nome de usuário
+                // Verify if the client has already registered with a username
+                if(!userNames.containsKey(clientSocketAddress)) {
+                    System.out.println("Cliente não registrado, favor registrar com /REG <nome de usuário>.");
+                    continue;
+                }
+
+                // Send messages
                 if(command.startsWith("/MSG ")) {
-                    if(!userNames.containsKey(clientSocketAddress)) {
-                        System.out.println("Cliente não registrado tentou enviar uma mensagem.");
-                        continue;
-                    }
-
                     if(command.substring(5).trim().split(" ").length < 2) {
-                        System.out.println("Mensagem inválida.");
+                        System.out.println("Mensagem inválida. Uso: /MSG <usuário> <mensagem>");
                         continue;
                     }
 
+                    // Get sender, receiver and the message
                     String sender = userNames.get(clientSocketAddress);
                     String receiver = command.substring(5).trim().split(" ")[0];
                     String msg = sender + ": " + command.substring(5 + receiver.length()).trim();
                     System.out.println(receiver + " <- " + msg);
 
+                    // Send the message to all users
                     if(receiver.toLowerCase().equals("all")) {
                         for(InetSocketAddress client : clients) {
                             if(!client.equals(clientSocketAddress)) {
@@ -79,6 +88,7 @@ public class ServerUDP {
                             }
                         }
                     }
+                    // Send the message to a specific user
                     else {
                         for(InetSocketAddress client : clients) {
                             if(userNames.get(client).equals(receiver)) {
@@ -94,6 +104,65 @@ public class ServerUDP {
                         }
                     }
                     continue;
+                }
+
+                // Send files
+                if (command.startsWith("/FILE ")) {
+                    if(command.substring(6).trim().split(" ").length < 2) {
+                        System.out.println("Comando inválido. Uso: /FILE <usuário> <arquivo>");
+                        continue;
+                    }
+
+                    // Get sender, receiver and the file name
+                    String splitCommand[] = command.substring(6).trim().split(" ");
+                    String sender = userNames.get(clientSocketAddress);
+                    String receiver = splitCommand[0];
+                    int numChunks = Integer.parseInt(splitCommand[splitCommand.length - 1]);
+                    int infoSize = splitCommand[splitCommand.length - 1].length();
+                    String fileName = command.substring(6 + receiver.length(), command.length() - infoSize).trim();
+                    System.out.println(sender + " -> " + receiver + " - envio de arquivo: " + fileName);
+
+                    // Receive the file
+                    FileOutputStream streamNewFile = new FileOutputStream("arquivo_teste_recebido");
+                    // BufferedOutputStream bufferNewFile = new BufferedOutputStream(streamNewFile);
+
+                    byte[] fileBuffer = new byte[8192]; // 8KB
+                    DatagramPacket packet = new DatagramPacket(fileBuffer, fileBuffer.length);
+
+                    for (int i = 0; i < numChunks; i++) {
+                        socket.receive(packet);
+                        String test = new String(packet.getData(), 0, packet.getLength());
+                        //System.out.println(test);
+                        streamNewFile.write(test.getBytes(), 0, test.getBytes().length * i);
+                        streamNewFile.flush();
+                    }
+                    // bufferNewFile.close();
+                    streamNewFile.close();
+
+                    // Send the file to all users
+                    // if(receiver.toLowerCase().equals("all")) {
+                    //     for(InetSocketAddress client : clients) {
+                    //         if(!client.equals(clientSocketAddress)) {
+                    //             DatagramPacket sendPacket = new DatagramPacket(
+                    //                     command.getBytes(),
+                    //                     command.length(),
+                    //                     client.getAddress(),
+                    //                     client.getPort()
+                    //             );
+                    //             socket.send(sendPacket);
+                    //         }
+                    //     }
+                    // }
+                    // Send the file to a specific user
+                    // else {
+                    //     for(InetSocketAddress client : clients) {
+                    //         if(userNames.get(client).equals(receiver)) {
+
+
+
+                    //         }
+                    //     }
+                    // }
                 }
 
                 if(command.startsWith("/FIM")) {
